5e7576b3096e6339c5a7b142e14f921c0acfc427
Daniel Lezcano <daniel.lezcano727@gmail.com>
2022-11-10 13:40:04 -0300

    React bootstrap

---
+++ src/home/Pie.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pie() {
+  return (
+    <div />
+  )
+}
+
+export default Pie;
---
+++ src/home/Seccion.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Seccion() {
+  return (
+    <div />
+  )
+}
+
+export default Seccion;
