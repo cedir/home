58dd3f38f0a80dc739c7fcddc0fda6b24d6bf9ea
DanielLezcano727 <daniel.lezcano727@gmail.com>
2022-12-06 12:53:52 -0300

    Renamed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../Navbar/Navbar';
+import Navbar from '../navbar/Navbar';
 import Homepage from '../home/Homepage';
 import Pie from '../home/Pie';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/App';
+import App from './app/App';
 import './styles/index.scss';
 import './styles/font-awesome-4.7.0/css/font-awesome.min.css';
 
