e5a6c06b3f0b1d7754475cf96d882a81fda58b7f
DanielLezcano727 <daniel.lezcano727@gmail.com>
2022-12-08 12:05:44 -0300

    Card

--- src/home/secciones/InfoCards.tsx
+++ src/home/secciones/InfoCards.tsx
@@ -1,5 +1,6 @@
-import { Grid } from '@mui/material';
+import { Card, CardContent, Grid } from '@mui/material';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import './styles.css';
 
 function InfoCards() {
@@ -14,10 +15,30 @@ function InfoCards() {
                     Prevención, diagnóstico y tratamiento de enfermedades digestivas.
                 </p>
             </Grid>
-            <Grid item xs={ 12 }>
-            </Grid>
-            <Grid item xs={ 12 }>
-                
+            <Grid item xs={ 10 }>
+                <Grid container>
+                    <Grid item md={ 3 }>
+                        <Card variant='outlined'>
+                            <CardContent>
+                                <h4 className='text-center' style={ { fontFamily: 'Raleway', fontWeight: 400 } }>Quienes Somos</h4>
+                                <div className='separator' />
+                                <p className='text-secondary'>Conozca nuestro equipo. Toda la calidad medica en un solo lugar.</p>
+                                <Button variant='primary' size='sm' className='cedirbutton'>
+                                    Leer más
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item md={ 3 }>
+
+                    </Grid>
+                    <Grid item md={ 3 }>
+
+                    </Grid>
+                    <Grid item md={ 3 }>
+
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
