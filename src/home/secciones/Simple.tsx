06274890cc2e9bd3aa4503dd9739ecc4eba0e266
DanielLezcano727 <daniel.lezcano727@gmail.com>
2022-12-09 11:07:04 -0300

    List of medics

--- src/home/secciones/Simple.tsx
+++ src/home/secciones/Simple.tsx
@@ -1,22 +1,24 @@
 import { Grid } from '@mui/material';
 import React from 'react';
-import { Button, Table } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 function Simple() {
     return (
         <Grid container justifyContent='center' sx={ { backgroundColor: '#373737' } }>
             <Grid item md={ 10 }>
-                <Grid container justifyContent='center' alignItems='center'>
+                <Grid container justifyContent='center' alignItems='center' className='py-3'>
                     <Grid item md={ 8 } className='text-center text-white'>
                         <h2>ENCUENTRE SU MÉDICO</h2>
-                        <Table style={ { color: '#cdcdcd' } }>
-                            <thead>
-                                <th>Gastroenterólogos</th>
-                                <th>Proctólogos</th>
-                                <th>Hepatólogos</th>
-                                <th>Médicos Clínicos</th>
-                            </thead>
-                        </Table>
+                        <Grid container alignItems='center' justifyContent='center'>
+                            { ['Gastroenterólogos', 'Proctólogos', 'Hepatólogos', 'Médicos Clínicos'].map((med, id) => (<span key={ id }>
+                                <Grid container item alignItems='center'>
+                                    <i className='fa fa-circle text-primary' style={ { fontSize: '4px', paddingRight: '1rem' } }/>
+                                    <span style={ { color: '#cdcdcd', paddingRight: '1.5rem' } }>
+                                        { med }
+                                    </span>
+                                </Grid>
+                            </span>)) }
+                        </Grid>
                     </Grid>
                     <Grid item md={ 4 } className='text-center'>
                         <Button className='px-4 py-2'>
